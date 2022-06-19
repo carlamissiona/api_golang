af989b98aba0764613069b057a5f925d6f7b430a
missiona.carla <missiona.carla@gmail.com>
2022-06-19 06:30:19 +0000

    error

--- clean/ports/patient.go
+++ clean/ports/patient.go
@@ -8,6 +8,7 @@ import (
 	"encoding/csv"
 	_ "github.com/gorilla/sessions"
 	"log"
+  "io"
 	"os"
 )
 
@@ -84,17 +85,36 @@ func data_todb(api *Service) {
 	// read csv values using csv.Reader
 	csv_reader := csv.NewReader(csvfile)
 	// data, err := csv_reader.ReadAll()
-	_, err = csv_reader.ReadAll()
-	if err != nil {
-		log.Printf("=>ERROR!! CANT READ CSV FILE \n %v \n=>END OF ERROR!! NO CSV FILE ", err)
-		handle_error_csv_read()
+	// _, err = csv_reader.ReadAll()
+	// if err != nil {
+	// 	log.Printf("=>ERROR!! CANT READ CSV FILE \n %v \n=>END OF ERROR!! NO CSV FILE ", err)
+	// 	handle_error_csv_read()
 
-	}
+	// }
 	log.Println("API data")
 	// log.Printf("=>DEBUG!! API DATA")
 	// log.Println(data)
 	// log.Printf("=>END OF DEBUG!! API DATA")
 	log.Printf("=>END OF DEBUG!! Insert to DB %v", api.Data.Connect_DB())
+
+    for {
+        each_record, err := csv_reader.Read()
+      	log.Printf("each_record %v", each_record)
+      	// log.Println("each_record error %v", err)
+        if err != nil || err == io.EOF {
+
+            log.Fatal(err)
+            break
+        }
+         var incr =0
+         for value := range each_record {
+            
+           log.Printf("This is a record of matrix %s\n  %v", each_record[value] , incr)
+            incr++
+         }
+    }
+  
+  
 	api.Data.Execute(`INSERT INTO covid_observations (the_country_name, the_state_name,confirmed_no,deaths_no,recovered_no,observation_date) VALUES('Mainland China','Anhui',102,10,2,timestamp '2015-01-10 17:00:00' );`)
 
 	log.Println(api.Data.Query(`Select * from covid_observations`))
